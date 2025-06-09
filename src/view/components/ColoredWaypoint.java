2a65ea971b64b6e49813a19d62d0f2465a8abedf
arthur <alwaysdecry@gmail.com>
2025-06-09 18:05:14 -0300

    trying to fix waypoints

---
+++ src/view/components/ColoredWaypoint.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package view.components;
+import org.jxmapviewer.viewer.DefaultWaypoint;
+import org.jxmapviewer.viewer.GeoPosition;
+import java.awt.Color;
+
+public class ColoredWaypoint extends DefaultWaypoint {
+    private final Color color;
+
+    public ColoredWaypoint(GeoPosition pos, Color color) {
+        super(pos);
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+}
