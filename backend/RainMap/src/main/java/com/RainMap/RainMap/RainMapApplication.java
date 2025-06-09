7bdde71d930e7bf65b40263e89265860bfb2f02c
saulopietro <saulo154@outlook.com>
2025-06-09 00:48:22 -0300

    start backend

---
+++ backend/RainMap/src/main/java/com/RainMap/RainMap/RainMapApplication.java
@@ -0,0 +1,13 @@
+package com.RainMap.RainMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RainMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RainMapApplication.class, args);
+	}
+
+}
